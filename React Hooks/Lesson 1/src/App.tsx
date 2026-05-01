3a014c68cd5162c7ada40bacef27d88e2a52fdee
usmanmobeen111 <thecodeweaver111@gmail.com>
2026-05-01 23:59:44 +0500

    useState, useEffect, useCallback, useMemo

---
+++ React Hooks/Lesson 1/src/App.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect, useCallback, useMemo } from "react"
+
+interface User {
+  id: number,
+  username: string
+}
+
+const App = () => {
+  // const [count, seCount] = useState<number | null>(0)
+  // const [user, setUser] = useState<User | null>(null)
+  // const [user, setUser] = useState<User[] | null>([])
+  const [count, seCount] = useState<number>(0)
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    console.log("Mounting")
+    console.log("Users: ", user)
+
+
+    return () => {
+      console.log("Unmounting")
+    }
+  }, [user])
+
+  const addOne = useCallback(
+    () => {
+      seCount(prev => prev + 1)
+    },
+    []
+  )
+
+  type fibFunc = (n: number) => number
+
+  const fibonacci: fibFunc = (n) => {
+    if (n < 2) return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+  }
+
+  const number: number = 12
+  const memoizedFib = useMemo<number>(() => fibonacci(number), [number])
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={addOne}>Add</button>
+      <button onClick={() => seCount(prev => prev - 1)}>Subtract</button>
+      <span>{memoizedFib}</span>
+    </>
+  )
+}
+
+export default App
